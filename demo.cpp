24eba5dde86d760dc8aa5262e19e7248cbeca31e
Kumar Ojaswi <kumarojaswi3236@gmail.com>
2024-07-04 13:26:51 +0530

    add demo

---
+++ demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Welcome to VSCode."<<endl;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    cout<<"The number entered is: "<<n<<endl;
+    return 0;
+}
