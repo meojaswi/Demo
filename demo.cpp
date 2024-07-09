847e432292777c0f15a905f13c92acb9aace4dc7
Kumar Ojaswi <kumarojaswi3236@gmail.com>
2024-07-09 10:28:12 +0530

    update demo with CPP course L-244

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <fstream>   //for file handling fstream header file is included.
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"Welcome to VSCode."<<endl;
-    cout<<"Enter a number"<<endl;
-    cin>>n;
-    cout<<"The number you entered is: "<<n<<endl;
-    return 0;
+    ofstream outfile ("my.txt",ios::trunc); //this ios::trunc mode removes the content                  of the my.txt file without
+                                         //erasing the data in it.
+
+//ios::trunc is the default mode we can use it without writing ios::trunc also.
+    
+    outfile<<"Hello"<<endl; //adding Hello in the my.txt file.
+    outfile <<"25"<<endl;   //adding 25 in the my.txt file.
+    outfile.close();  // this command closes the stream and the file is free form the program.
 }
